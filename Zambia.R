6d4bddcd066e0c082f487f2169442dcd1abe70e8
dmecklin <46754064+dmecklin@users.noreply.github.com>
2019-05-03 00:15:48 -0400

    Delete Zambia.R

--- Zambia.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(data.table)
-# becuase data set is big 
-CLEA <- clea_lc_20181119
-Africa <- filter(CLEA, rg == "Africa")
-Zambia <- filter(Africa, ctr_n == "Zambia")
-Zambia_multiparty <- filter(Zambia, yr <= 1991 | yr <= 1973)
-# one party state from 1973-'91
